f86baa6fb6cf3d41a2a4a8f258e056a0e4c66023
Osiris Arguello <osirisarguello@gmail.com>
2018-09-16 22:25:17 -0300

    -Agregado menu y botones a las diferentes funcionalidades iniciales

--- app/src/main/java/com/tdp2/quechuaapp/MainActivity.java
+++ app/src/main/java/com/tdp2/quechuaapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.tdp2.quechuaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tdp2.quechuaapp.student.CourseSignUpActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        attachEvents();
+
+    }
+
+    private void attachEvents() {
+
+        Button studentSignUpButton = findViewById(R.id.student_signup_button);
+
+        studentSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent courseSignUpActivity = new Intent(getApplicationContext(), CourseSignUpActivity.class);
+                startActivity(courseSignUpActivity);
+            }
+        });
     }
 }
