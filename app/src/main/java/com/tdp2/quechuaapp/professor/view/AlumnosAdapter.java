81e1055391ecc78a7959638a6750ff0b933b7ee7
lucia.capon <lucia.capon@fdvsolutions.com>
2018-09-23 22:57:35 -0300

    Nuevo layout para celda de estudiantes

--- app/src/main/java/com/tdp2/quechuaapp/professor/view/AlumnosAdapter.java
+++ app/src/main/java/com/tdp2/quechuaapp/professor/view/AlumnosAdapter.java
@@ -9,11 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.tdp2.quechuaapp.R;
 import com.tdp2.quechuaapp.model.Alumno;
 
 public class AlumnosAdapter extends ArrayAdapter<Alumno> {
+
+    // TODO: Tiene que haber otra manera de hacer esto
+    public Boolean condicionales = false;
 
     public AlumnosAdapter(@NonNull Context context, @NonNull List<Alumno> listaAlumnos) {
         super(context, 0,  listaAlumnos);
@@ -24,14 +29,22 @@ public class AlumnosAdapter extends ArrayAdapter<Alumno> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         final Alumno alumno = getItem(position);
 
-        // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1,
-                    parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.estudiante_view, parent, false);
         }
 
-        TextView textView = convertView.findViewById(android.R.id.text1);
-        textView .setText("Nombre: " + alumno.apellido + ", " + alumno.nombre);
+        TextView textView = convertView.findViewById(R.id.idEstudiante);
+        textView.setText(alumno.padron + " - " + alumno.apellido + ", " + alumno.nombre);
+
+        Button button = convertView.findViewById(R.id.inscribirButton);
+        button.setVisibility(condicionales ? View.VISIBLE : View.INVISIBLE);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Si el estudiante es "condicional", inscribir
+            }
+        });
+
         return convertView;
     }
 
