549a8011d81c49d4bb92b23445618e00459708f6
Osiris Argüello <osirisarguello@gmail.com>
2018-10-20 18:50:15 -0300

    QuechuaApp-18 Implementación de la desinscripcion

--- app/src/main/java/com/tdp2/quechuaapp/networking/EstudianteApi.java
+++ app/src/main/java/com/tdp2/quechuaapp/networking/EstudianteApi.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
@@ -18,6 +19,11 @@ public interface EstudianteApi {
     @GET("/public/materias/{materiaId}/cursos")
     Call<ArrayList<Curso>> getCursosPorMateria(@Path("materiaId")Integer materiaId);
 
-    @POST("/public/inscripcion-cursos/{cursoId}/{alumnoId}")
-    Call<Inscripcion> inscribirAlumno(@Path("alumnoId")Integer alumnoId, @Path("cursoId")Integer cursoId);
+    @POST("/api/inscripcion-cursos/{cursoId}")
+    Call<Inscripcion> inscribirAlumno(@Header("Authorization")String apiToken, @Path("cursoId")Integer cursoId);
+
+    @POST("/api/inscripcion-cursos/{cursoId}/accion/desinscribir")
+    Call<Inscripcion> desinscribirAlumno(@Header("Authorization")String apiToken, @Path("cursoId")Integer inscripcionId);
+
+
 }
--- app/src/main/java/com/tdp2/quechuaapp/networking/EstudianteService.java
+++ app/src/main/java/com/tdp2/quechuaapp/networking/EstudianteService.java
@@ -3,6 +3,7 @@ package com.tdp2.quechuaapp.networking;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.tdp2.quechuaapp.login.model.UserSessionManager;
 import com.tdp2.quechuaapp.model.Curso;
 import com.tdp2.quechuaapp.model.Inscripcion;
 
@@ -18,6 +19,7 @@ import retrofit2.Response;
 
 public class EstudianteService {
 
+    public static final String AUTHORIZATION_PREFIX = "Bearer ";
     private EstudianteApi estudianteApi;
 
     public EstudianteService() {
@@ -55,7 +57,7 @@ public class EstudianteService {
         });
     }
 
-    public void getCursosPorMateria(Integer idEstudiante, Integer idMateria, final Client client){
+    public void getCursosPorMateria(Integer idMateria, final Client client){
         estudianteApi.getCursosPorMateria(idMateria).enqueue(new Callback<ArrayList<Curso>>() {
 
             @Override
@@ -86,8 +88,57 @@ public class EstudianteService {
         });
     }
 
-    public void inscribirAlumno(Integer idAlumno, Integer idCurso, final Client client){
-        estudianteApi.inscribirAlumno(idAlumno,idCurso).enqueue(new Callback<Inscripcion>() {
+    public void inscribirAlumno(Integer idCurso, final Client client){
+        String apiToken=new UserSessionManager(client.getContext()).getAuthorizationToken();
+        estudianteApi.inscribirAlumno(AUTHORIZATION_PREFIX+apiToken,idCurso).enqueue(new Callback<Inscripcion>() {
+
+            @Override
+            public void onResponse(Call<Inscripcion> call, Response<Inscripcion> response) {
+                if (response.code() > 199 && response.code() < 300) {
+                    if(response.body() != null) {
+                        Log.i("ESTUDIANTESERVICE", response.body().toString());
+                        client.onResponseSuccess(response.body());
+                    }else {
+                        Log.i("ESTUDIANTESERVICE", "NO RESPONSE");
+                        client.onResponseError(null);
+                    }
+                } else {
+                    if(response.body() != null) {
+                        Log.e("ESTUDIANTESERVICE", response.body().toString());
+                    }else {
+                        Log.e("ESTUDIANTESERVICE", "NO RESPONSE");
+                    }
+                    if (response.code() == 400) {
+                        Converter<ResponseBody, ApiError> converter =
+                                ApiClient.getInstance().getRetrofit().responseBodyConverter(ApiError.class, new Annotation[0]);
+
+                        ApiError error;
+
+                        try {
+                            error = converter.convert(response.errorBody());
+                            Log.e("error message", error.message);
+                            client.onResponseError(error.message);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        client.onResponseError(null);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Inscripcion> call, Throwable t) {
+                Log.e("ESTUDIANTESERVICE", t.getMessage());
+                client.onResponseError(null);
+            }
+        });
+    }
+
+    public void desinscribirAlumno(Integer idCurso, final Client client){
+        String apiToken=new UserSessionManager(client.getContext()).getAuthorizationToken();
+        estudianteApi.desinscribirAlumno(AUTHORIZATION_PREFIX+apiToken,idCurso).enqueue(new Callback<Inscripcion>() {
 
             @Override
             public void onResponse(Call<Inscripcion> call, Response<Inscripcion> response) {
